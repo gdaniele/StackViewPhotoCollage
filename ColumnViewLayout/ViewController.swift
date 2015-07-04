ea9a4f1a00291596f7b52559595d3ba22219128d
Giancarlo Daniele <giancarlo@cs.stanford.edu>
2015-07-04 13:01:31 -0700

    Initial Commit

---
+++ ColumnViewLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColumnViewLayout
+//
+//  Created by Giancarlo on 7/4/15.
+//  Copyright (c) 2015 Giancarlo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

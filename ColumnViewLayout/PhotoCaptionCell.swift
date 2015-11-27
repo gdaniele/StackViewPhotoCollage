ab3c8731b4b0c41a22c4f980e6808746ca8aca94
Giancarlo Daniele <gdaniele@stanford.edu>
2015-11-27 16:23:48 -0600

    Adds PureLayout

--- ColumnViewLayout/PhotoCaptionCell.swift
+++ ColumnViewLayout/PhotoCaptionCell.swift
@@ -10,24 +10,24 @@ import UIKit
 
 // Inspired by http://www.raywenderlich.com/99146/video-tutorial-custom-collection-view-layouts-part-1-pinterest-basic-layout
 class PhotoCaptionCell: UICollectionViewCell {
-	// MARK:- Layout Concerns
-	private var imageViewHeightLayoutConstraint: NSLayoutConstraint
-
 	// MARK:- Lazy UI
-	private let imageView: UIImageView = {
+	private lazy var imageView: UIImageView = {
 		let imageView = UIImageView()
 		imageView.contentMode = .ScaleAspectFit
 		imageView.translatesAutoresizingMaskIntoConstraints = false
 		imageView.backgroundColor = UIColor.orangeColor()
 		return imageView
 	}()
-	
-	private let mainView: UIView = {
-		let view = UIView()
-		view.translatesAutoresizingMaskIntoConstraints = false
+	private lazy var mainView: UIView = {
+		let view = UIStackView(arrangedSubviews: [self.imageView, self.titleLabel])
+		
+		view.axis = UILayoutConstraintAxis.Vertical
+		view.distribution = UIStackViewDistribution.EqualCentering
+		view.alignment = UIStackViewAlignment.Leading
+		
 		return view
 	}()
-	private let titleLabel: UILabel = {
+	private lazy var titleLabel: UILabel = {
 		let label = UILabel()
 		label.font = UIFont(name: "AvenirMedium", size: 13)
 		label.textAlignment = .Center
@@ -44,47 +44,25 @@ class PhotoCaptionCell: UICollectionViewCell {
         }
     }
 	
-	override init(frame: CGRect) {
-		self.imageViewHeightLayoutConstraint = NSLayoutConstraint(item: self.imageView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 200)
-		super.init(frame: CGRectZero)
-	}
-
-	required init?(coder aDecoder: NSCoder) {
-	    fatalError("init(coder:) has not been implemented")
-	}
-	
 	override func layoutSubviews() {
 		self.backgroundColor = UIColor.yellowColor()
-		// Construct mainView
-		self.mainView.addSubview(self.imageView)
-		self.mainView.addSubview(self.titleLabel)
 		
 		// Add views to contentView
-		self.contentView.addSubview(self.mainView)
-		self.contentView.translatesAutoresizingMaskIntoConstraints = false
+		self.addSubview(self.mainView)
 		
 		// Set mainViewContstraints
-		self.addConstraint(NSLayoutConstraint(item: self.mainView, attribute: .Top, relatedBy: .Equal, toItem: self.contentView, attribute: .Top, multiplier: 1, constant: 0))
-		self.addConstraint(NSLayoutConstraint(item: self.mainView, attribute: .Bottom, relatedBy: .Equal, toItem: self.contentView, attribute: .Bottom, multiplier: 1, constant: 0))
-		self.addConstraint(NSLayoutConstraint(item: self.mainView, attribute: .Leading, relatedBy: .Equal, toItem: self.contentView, attribute: .Leading, multiplier: 1, constant: 0))
-		self.addConstraint(NSLayoutConstraint(item: self.mainView, attribute: .Trailing, relatedBy: .Equal, toItem: self.contentView, attribute: .Trailing, multiplier: 1, constant: 0))
-
-		/// Set imageView constraints
-		self.addConstraint(self.imageViewHeightLayoutConstraint)
-		self.addConstraint(NSLayoutConstraint(item: self.imageView, attribute: .Leading, relatedBy: .Equal, toItem: self.contentView, attribute: .Leading, multiplier: 1, constant: 0))
-		self.addConstraint(NSLayoutConstraint(item: self.imageView, attribute: .Top, relatedBy: .Equal, toItem: self.contentView, attribute: .Top, multiplier: 1, constant: 0))
-		self.addConstraint(NSLayoutConstraint(item: self.imageView, attribute: .Trailing, relatedBy: .Equal, toItem: self.contentView, attribute: .Trailing, multiplier: 1, constant: 0))
+		self.addConstraints(
+			NSLayoutConstraint.constraintsWithVisualFormat("V:[stackView]", options: .AlignAllLeft, metrics: nil, views: ["stackView": self.mainView])
+		)
 		
-		/// Set titleLabel constraints
-		self.addConstraint(NSLayoutConstraint(item: self.titleLabel, attribute: .Leading, relatedBy: .Equal, toItem: self.contentView, attribute: .Leading, multiplier: 1, constant: 0))
-		self.addConstraint(NSLayoutConstraint(item: self.titleLabel, attribute: .Trailing, relatedBy: .Equal, toItem: self.contentView, attribute: .Trailing, multiplier: 1, constant: 0))
-		self.addConstraint(NSLayoutConstraint(item: self.titleLabel, attribute: .Bottom, relatedBy: .Equal, toItem: self.contentView, attribute: .Bottom, multiplier: 1, constant: 0))
-		self.addConstraint(NSLayoutConstraint(item: self.titleLabel, attribute: .Top, relatedBy: .Equal, toItem: self.imageView, attribute: .Bottom, multiplier: 1, constant: 0))
+		self.addConstraints(
+			NSLayoutConstraint.constraintsWithVisualFormat("H:[stackView]", options: .AlignAllLeft, metrics: nil, views: ["stackView": self.mainView])
+		)
 	}
 	
     override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
         super.applyLayoutAttributes(layoutAttributes)
-        let attributes = layoutAttributes as! TwoColumnLayoutAttributes
-		self.imageViewHeightLayoutConstraint.constant = attributes.photoHeight
+//        let attributes = layoutAttributes as! TwoColumnLayoutAttributes
+//		self.imageViewHeightLayoutConstraint.constant = attributes.photoHeight
     }
 }

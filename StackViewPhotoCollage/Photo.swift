aacff870271f044ea0466fa0754fd92b57f8ceeb
Giancarlo Daniele <gdaniele@stanford.edu>
2015-11-28 12:02:56 -0600

    Adds height constraint update for calculated photo height

--- StackViewPhotoCollage/Photo.swift
+++ StackViewPhotoCollage/Photo.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-let comments = ["Lorem ipsum dolor sit amet, consectetur adipiscing",
+let comments = ["Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing",
 	"elit, sed do eiusmod tempor incididunt ut labore",
 	"et dolore magna aliqua. Ut enim ad minim veniam",
 	"quis nostrud exercitation ullamco laboris nisi ut",
--- StackViewPhotoCollage/StackViewPhotoCollage/PhotoCaptionCell.swift
+++ StackViewPhotoCollage/StackViewPhotoCollage/PhotoCaptionCell.swift
@@ -12,13 +12,14 @@ import UIKit
 class PhotoCaptionCell: UICollectionViewCell {
 	// MARK:- Layout Concerns
 	private var textHeightLayoutConstraint: NSLayoutConstraint?
-	
+	private var photoHeightLayoutConstraint: NSLayoutConstraint?
+
 	// MARK:- Lazy UI
 	private lazy var imageView: UIImageView = {
 		let imageView = UIImageView()
 		imageView.contentMode = .ScaleAspectFill
 		imageView.translatesAutoresizingMaskIntoConstraints = false
-
+		
 		return imageView
 	}()
 	private lazy var mainView: UIView = {
@@ -29,10 +30,10 @@ class PhotoCaptionCell: UICollectionViewCell {
 		view.alignment = UIStackViewAlignment.Fill
 		view.layoutMarginsRelativeArrangement = true
 		view.translatesAutoresizingMaskIntoConstraints = false
-
+		
 		return view
 	}()
-	var titleFont: UIFont = UIFont(name: "Avenir", size: 15)!
+	var titleFont: UIFont = UIFont(name: "Avenir", size: 15)! // set default font
 	private lazy var titleLabel: UILabel = {
 		let label = UILabel()
 		label.font = self.titleFont
@@ -40,10 +41,10 @@ class PhotoCaptionCell: UICollectionViewCell {
 		label.numberOfLines = 0
 		label.lineBreakMode = .ByWordWrapping
 		label.translatesAutoresizingMaskIntoConstraints = false
-
+		
 		return label
 	}()
-
+	
 	// MARK:- Public API
 	var image: UIImage? {
 		didSet {
@@ -62,11 +63,13 @@ class PhotoCaptionCell: UICollectionViewCell {
 	
 	override init(frame: CGRect) {
 		super.init(frame: CGRectZero)
+		
+		self.photoHeightLayoutConstraint = NSLayoutConstraint(item: imageView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 200)
 		self.textHeightLayoutConstraint = NSLayoutConstraint(item: titleLabel, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 30)
 	}
-
+	
 	required init?(coder aDecoder: NSCoder) {
-	    fatalError("init(coder:) has not been implemented")
+		fatalError("init(coder:) has not been implemented")
 	}
 	
 	override func layoutSubviews() {
@@ -86,17 +89,28 @@ class PhotoCaptionCell: UICollectionViewCell {
 				]
 			)
 		}
-
+		
+		// Add default padding around title label
+		if let uSuperView = self.titleLabel.superview {
+			self.addConstraints(
+				[
+					NSLayoutConstraint(item: self.titleLabel, attribute: .Leading, relatedBy: .Equal, toItem: uSuperView, attribute: .Leading, multiplier: 1, constant: 5),
+					NSLayoutConstraint(item: self.titleLabel, attribute: .Trailing, relatedBy: .Equal, toItem: uSuperView, attribute: .Trailing, multiplier: 1, constant: -5)
+				]
+			)
+		}
+		
 		if let uHeightConstraint = self.textHeightLayoutConstraint {
 			self.addConstraint(uHeightConstraint)
 		}
 	}
 	
-    override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
-        super.applyLayoutAttributes(layoutAttributes)
+	override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
+		super.applyLayoutAttributes(layoutAttributes)
 		
 		if let attributes = layoutAttributes as? MultipleColumnLayoutAttributes {
+			photoHeightLayoutConstraint?.constant = attributes.photoHeight
 			textHeightLayoutConstraint?.constant = attributes.annotationHeight
 		}
-    }
+	}
 }
